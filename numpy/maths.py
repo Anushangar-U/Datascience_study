ef6eb90a911bdc16541d25df6c3b395cfe51241d
Anushangar-U <u.anushangar@gmail.com>
2026-01-06 19:30:07 +0530

    maths

---
+++ numpy/maths.py
@@ -0,0 +1,24 @@
+from math import cos
+import numpy as np
+
+a = np.array([1,2,3,4])
+b = a + 2
+print(b)
+
+c = a - 2
+print(c)    
+
+d = a * 2
+print(d)
+
+e = a / 2
+print(e)
+
+f = a ** 2
+print(f)
+
+g = a % 2
+print(g)
+
+h = cos(a)
+print(h)
