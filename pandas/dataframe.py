41a2432f34b5b434e47fde7e16f046910042a338
Anushangar-U <u.anushangar@gmail.com>
2026-02-02 15:04:32 +0530

    indexing

---
+++ pandas/dataframe.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv("python\ds_study\pandas\orders.csv")
+print(df.head())
+
+
+
+
--- pandas/starting_pandas.py
+++ pandas/starting_pandas.py
@@ -1,8 +1,14 @@
 import pandas as pd
 import numpy as np
 
-s = pd.Series([1,3,5,np.nan,6,8])
+s = pd.Series([1,3,5,np.nan,7,8]) 
 print(s)
 
-dates = pd.date_range("20130101",periods=6)
+ap = pd.Series([1,3,5,np.nan,7,8], index=['a','b','c','d','e','f'])
+print(ap)
+print(ap.loc['c']) # label based indexing
+print(ap.iloc[2])  # location based indexing
+print(ap[ap>5]) # conditional selection  
+
+dates = pd.date_range("20130101",periods=6) 
 print(dates)
